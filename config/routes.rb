80a7c5e88851ce434a2ae32d223b30e787d511b7
Adel Koljenovic <adel.koljenovic@outlook.com>
2018-08-30 02:22:41 -0400

    create routes & actions for all controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,25 @@
 Rails.application.routes.draw do
   # STEP 1: A ROUTE triggers a controller action
   # verb "/urls" => "namespace/controllers#action"
+  namespace :api do
+    #records
+    get "/records" => "records#index"
+    post "/records" => "records#create"
+    get "/records/:id" => "records#show"
+    patch "/records/:id" => "records#update"
+    delete "/records/:id" => "records#destroy"
+    #vehicles
+    get "/vehicles" => "vehicles#index"
+    post "/vehicles" => "vehicles#create"
+    delete "/vehicles/:id" => "vehicles#destroy"
+    #shops
+    get "/shops" => "shops#index"
+    post "/shops" => "shops#create"
+    get "/shops/:id" => "shops#show"
+    patch "/shops/:id" => "shops#update"
+    delete "/shops/:id" => "shops#destroy"
+    #users/user-related
+    get "/users" => "users#index"
+    post "/users" => "users#create"
+    post "/sessions" => "sessions#create"
 end
